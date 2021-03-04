0dff8b3f697370fee69ac2c879c01d9bbcf82015
kellenwiltshire <kellenwiltshire@outlook.com>
2021-03-04 12:08:10 -0500

    pagination working

--- components/Layout/Pagination.js
+++ components/Layout/Pagination.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 function Pagination({ currentCursor, name, cat }) {
+	const router = useRouter();
 	return (
 		<div className='w-full flex justify-center m-10'>
 			<ul className='flex'>
-				<li className='mx-1 px-3 py-2 bg-gray-200 text-gray-500 rounded-lg'>
-					<Link
-						href={{
-							pathname: `Shop/${cat}`,
-							query: { cat: cat, name: name, cursor: currentCursor },
-						}}
+				<li className='mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg cursor-pointer'>
+					<div
+						onClick={() => router.back()}
+						className='flex items-center font-bold'
 					>
-						<div className='flex items-center font-bold'>
-							<span className='mx-1'>previous</span>
-						</div>
-					</Link>
+						<span className='mx-1'>Previous</span>
+					</div>
 				</li>
 
-				<li className='mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg'>
+				<li className='mx-1 px-3 py-2 bg-gray-200 text-gray-700 hover:bg-gray-700 hover:text-gray-200 rounded-lg cursor-pointer'>
 					<Link
 						href={{
 							pathname: `Shop/${cat}`,
