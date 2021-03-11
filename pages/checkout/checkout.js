6a721d395b187e1d00148141ea44d9d89639e32d
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-03-10 19:07:14 -0500

    Nearly ready for checkout test

--- pages/checkout/checkout.js
+++ pages/checkout/checkout.js
@@ -14,11 +14,21 @@ export default function Checkout({ cart, setCart }) {
 		router.push('/checkout/checkout');
 	};
 
-	let itemIDs = [];
-	console.log('ItemIDS: ', itemIDs);
+	let lineItems = [];
+	console.log(lineItems);
+	const initCheckout = () => {
+		handleCheckout(lineItems);
+	};
 
-	const handleCheckout = () => {
-		fetch('/LOCALHOST:4000/checkout');
+	const handleCheckout = (lineItems) => {
+		fetch('/LOCALHOST:4000/checkout', {
+			method: 'post',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({
+				lineItems: lineItems,
+				orderID: orderID,
+			}),
+		});
 	};
 
 	if (cart) {
@@ -27,6 +37,10 @@ export default function Checkout({ cart, setCart }) {
 				<Headers title='Checkout' />
 				<div className='container flex flex-wrap'>
 					{cart.map((list, i) => {
+						lineItems.push({
+							quantity: cart[i].quantity,
+							catalogObjectId: cart[i].item.object.itemData.variations[0].id,
+						});
 						return (
 							<CheckoutCard item={cart[i]} index={i} deleteItem={deleteItem} />
 						);
