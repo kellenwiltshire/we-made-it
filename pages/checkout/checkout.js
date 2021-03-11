4c37a4d666b40bfd92518dc922696c902b00a9d4
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-03-11 15:40:35 -0500

    Checkout coming along

--- pages/checkout/checkout.js
+++ pages/checkout/checkout.js
@@ -7,6 +7,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 export default function Checkout({ cart, setCart }) {
 	const orderID = uuidv4();
+	console.log(orderID);
 	const router = useRouter();
 	const deleteItem = (index) => {
 		cart.splice(index, 1);
@@ -16,19 +17,16 @@ export default function Checkout({ cart, setCart }) {
 
 	let lineItems = [];
 	console.log(lineItems);
-	const initCheckout = () => {
-		handleCheckout(lineItems);
-	};
 
-	const handleCheckout = (lineItems) => {
-		fetch('/LOCALHOST:4000/checkout', {
+	const handleCheckout = () => {
+		fetch('LOCALHOST:4000/checkout', {
 			method: 'post',
 			headers: { 'Content-Type': 'application/json' },
 			body: JSON.stringify({
 				lineItems: lineItems,
 				orderID: orderID,
 			}),
-		});
+		}).catch((err) => console.log(err));
 	};
 
 	if (cart) {
@@ -36,6 +34,12 @@ export default function Checkout({ cart, setCart }) {
 			<Layout cart={cart}>
 				<Headers title='Checkout' />
 				<div className='container flex flex-wrap'>
+					<button
+						onClick={handleCheckout}
+						className='mx-1 px-3 py-2 bg-purple-200 text-gray-700 hover:bg-purple-700 hover:text-gray-200 rounded-lg cursor-pointer'
+					>
+						Continue to Checkout
+					</button>
 					{cart.map((list, i) => {
 						lineItems.push({
 							quantity: cart[i].quantity,
