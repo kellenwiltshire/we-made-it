81c9eae1928ebf658641391f809d501e613af36f
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-03-14 14:06:15 -0400

    Ready for New Deployment

--- components/About/Location.js
+++ components/About/Location.js
@@ -3,25 +3,25 @@ import React from 'react';
 function Location() {
 	return (
 		<div className='w-full lg:w-1/3 flex justify-center my-5'>
-			<div class='bg-white shadow overflow-hidden sm:rounded-lg container'>
-				<div class='px-4 py-5 sm:px-6'>
-					<h3 class='text-2xl leading-6 font-medium text-dark-purple text-center'>
+			<div className='bg-white shadow overflow-hidden sm:rounded-lg container'>
+				<div className='px-4 py-5 sm:px-6'>
+					<h3 className='text-2xl leading-6 font-medium text-dark-purple text-center'>
 						Store Information
 					</h3>
 				</div>
-				<div class='border-t border-purple-200'>
+				<div className='border-t border-purple-200'>
 					<dl>
-						<div class='bg-purple-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-							<dt class='text-lg font-medium text-black'>Location</dt>
-							<dd class='mt-1 text-lg text-gray-900 sm:mt-0 sm:col-span-2'>
+						<div className='bg-purple-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+							<dt className='text-lg font-medium text-black'>Location</dt>
+							<dd className='mt-1 text-lg text-gray-900 sm:mt-0 sm:col-span-2'>
 								<p>101 King Ave E</p>
 								<p>Newcastle, On</p>
 								<p>L1B 1E3</p>
 							</dd>
 						</div>
-						<div class='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-							<dt class='text-lg font-medium text-black'>Hours</dt>
-							<ul class='mt-1 text-lg text-gray-900 sm:mt-0 sm:col-span-2'>
+						<div className='bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+							<dt className='text-lg font-medium text-black'>Hours</dt>
+							<ul className='mt-1 text-lg text-gray-900 sm:mt-0 sm:col-span-2'>
 								<li>Monday Closed</li>
 								<li>Tuesday 10:00 a.m. - 5:00 p.m.</li>
 								<li>Wednesday 10:00 a.m. - 5:00 p.m.</li>
@@ -31,9 +31,9 @@ function Location() {
 								<li>Sunday 10:00 a.m. - 5:00 p.m.</li>
 							</ul>
 						</div>
-						<div class='bg-purple-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
-							<dt class='text-lg font-medium text-black'>Contact</dt>
-							<dd class='mt-1 text-lg text-gray-900 sm:mt-0 sm:col-span-2'>
+						<div className='bg-purple-200 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'>
+							<dt className='text-lg font-medium text-black'>Contact</dt>
+							<dd className='mt-1 text-lg text-gray-900 sm:mt-0 sm:col-span-2'>
 								<div>
 									<a href='tel: 1 905 987 7266'>1 (905) 987 7266</a>
 								</div>
