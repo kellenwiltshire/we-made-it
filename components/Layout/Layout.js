3519f0232222886e5b8ea53d2340705a258f0206
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-03-22 15:48:29 -0400

    Final Changes Before Deployment

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -9,7 +9,8 @@ function Layout({ title, children, cart }) {
 			<Head>
 				<title>{title}</title>
 				<link rel='icon' href='/favicon.ico' />
-				<meta name='We-Made-It We Made It Newcastle Ontario Homemade Handmade Decor Local Clarington Canada Bowmanville Durham Oshawa' />
+				<meta description='We-Made-It We Made It Newcastle Ontario Homemade Handmade Decor Boutique Local Clarington Canada Bowmanville Durham Oshawa' />
+				<meta name='We Made It Local Handmade Boutique' />
 				<meta lang='en' />
 			</Head>
 			<Navigation cart={cart} />
