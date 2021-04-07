0405e33892ee6bd0eed773864056c9e43a7804de
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-04-07 17:46:52 -0400

    Checkout page updated

--- pages/checkout/checkoutCompleted.js
+++ pages/checkout/checkoutCompleted.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Headers from '../../components/Layout/Headers';
 import Layout from '../../components/Layout/Layout';
+import Location from '../../components/About/Location';
 
 export default function ShopCategories({ cart, setCart }) {
 	const resetCart = () => {
 		setCart([]);
 	};
 
-	resetCart();
+	useEffect(() => {
+		resetCart();
+	}, []);
 
 	return (
 		<Layout cart={cart} title='Checkout Completed || We Made It'>
 			<Headers title='Checkout Completed!' />
-			<div>
-				<p className='font-body'>Thank You So Much for Your Purchase!</p>
+			<div className='w-full flex flex-col justify-center'>
+				<div className='container px-5 py-24 mx-auto'>
+					<div className='xl:w-1/2 lg:w-3/4 w-full mx-auto text-center'>
+						<p className='leading-relaxed text-lg'>
+							Thank you so much for your Order and for supporting Local! You can
+							pick up your Order at the address below!
+						</p>
+
+						<span className='inline-block h-1 w-10 rounded bg-indigo-500 mt-8 mb-6'></span>
+						<h2 className='text-gray-900 font-medium title-font tracking-wider text-sm'>
+							Amanda Van Goor
+						</h2>
+						<p className='text-gray-500'>Owner</p>
+					</div>
+				</div>
+				<div className='w-full flex justify-center'>
+					<Location />
+				</div>
 			</div>
 		</Layout>
 	);
