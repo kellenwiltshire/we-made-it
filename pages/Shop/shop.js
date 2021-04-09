6191573ba991a8ddd04f3f985cbf0e74340e7bc3
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-04-09 17:16:33 -0400

    Working on new catalog

--- pages/Shop/shop.js
+++ pages/Shop/shop.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import ReactPaginate from 'react-paginate';
 import Headers from '../../components/Layout/Headers';
 import Layout from '../../components/Layout/Layout';
 import ProductCards from '../../components/Product/ProductCards';
@@ -8,6 +9,9 @@ import Pagination from '../../components/Layout/Pagination';
 export default function ShopCategories({ data, cart }) {
 	if (data) {
 		const [items, setItems] = useState([]);
+		const [currItems, setCurrItems] = useState([]);
+		const [currPage, setCurrPage] = useState(1);
+		const [numPages, setNumPages] = useState(1);
 		const dataItems = data.items;
 		let itemsWithPictures = [];
 		for (let i = 0; i < dataItems.length; i++) {
@@ -15,17 +19,43 @@ export default function ShopCategories({ data, cart }) {
 				itemsWithPictures.push(dataItems[i]);
 			}
 		}
+		const getCurrItems = (page) => {
+			const start = page * 50 - 50;
+			const currentItems = items.slice(start, start + 50);
+			return currentItems;
+		};
+		const handlePageChange = (e) => {
+			e.preventDefault();
+			const page = e.selected + 1;
+			setCurrPage(page);
+			const start = e.selected * 50 - 50;
+			const currentItems = items.slice(start, start + 50);
+			setCurrItems(currentItems);
+		};
+
 		useEffect(() => {
 			setItems(itemsWithPictures);
-		}, [data]);
-		let currentCursor = data.items.cursor;
+			setCurrItems(getCurrItems(currPage));
+			setNumPages(itemsWithPictures.length / 50);
+		}, [currPage]);
+		console.log('Current Items: ', currItems);
+		console.log('All Items: ', items);
+		console.log(numPages);
 		return (
 			<Layout cart={cart} title={`Shop || We Made It`}>
 				<Headers title='Shop' />
 				{/* <CategorySelect /> */}
 				{/* <Pagination currentCursor={currentCursor} name={name} cat={cat} /> */}
+				<div className='w-full flex justify-center'>
+					<ReactPaginate
+						pageCount={numPages}
+						onPageChange={handlePageChange}
+						pageRangeDisplayed={5}
+						marginPagesDisplayed={2}
+					/>
+				</div>
 				<div className='container m-1 lg:m-5 flex flex-row flex-wrap justify-center w-full font-body'>
-					{items.map((list, i) => {
+					{currItems.map((list, i) => {
 						let price = (
 							items[i].itemData.variations[0].itemVariationData.priceMoney
 								.amount / 100
@@ -35,7 +65,6 @@ export default function ShopCategories({ data, cart }) {
 							<ProductCards
 								item={items[i]}
 								title={items[i].itemData.name}
-								key={items[i].id}
 								itemID={items[i].id}
 								price={price}
 								defaultImage='/pictureComingSoon.png'
@@ -75,45 +104,4 @@ export async function getStaticProps() {
 			props: {},
 		};
 	}
-
-	// if (cursor) {
-	// 	try {
-	// 		const res = await fetch('https://we-made-it-api.herokuapp.com/catalog', {
-	// 			method: 'post',
-	// 			headers: { 'Content-Type': 'application/json' },
-	// 			body: JSON.stringify({
-	// 				cursor: cursor,
-	// 			}),
-	// 		});
-	// 		const data = await res.json();
-
-	// 		return {
-	// 			props: { data, cat, name },
-	// 		};
-	// 	} catch (error) {
-	// 		const data = error;
-	// 		return {
-	// 			props: { cat, name },
-	// 		};
-	// 	}
-	// } else {
-	// 	try {
-	// 		const res = await fetch('https://we-made-it-api.herokuapp.com/catalog', {
-	// 			method: 'post',
-	// 			headers: { 'Content-Type': 'application/json' },
-	// 			body: JSON.stringify({
-	// 				cursor: null,
-	// 			}),
-	// 		});
-	// 		const data = await res.json();
-	// 		return {
-	// 			props: { data, cat, name },
-	// 		};
-	// 	} catch (error) {
-	// 		const data = error;
-	// 		return {
-	// 			props: { cat, name },
-	// 		};
-	// 	}
-	// }
 }
