b7da09a6ff619304d8211a7b0590f5c2766ccdf7
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-04-11 12:17:03 -0400

    Catalog Loading Properly

--- pages/Shop/shop.js
+++ pages/Shop/shop.js
@@ -4,7 +4,6 @@ import Headers from '../../components/Layout/Headers';
 import Layout from '../../components/Layout/Layout';
 import ProductCards from '../../components/Product/ProductCards';
 import CategorySelect from '../../components/Categories/CategorySelect';
-import Pagination from '../../components/Layout/Pagination';
 
 export default function ShopCategories({ data, cart }) {
 	if (data) {
@@ -13,8 +12,10 @@ export default function ShopCategories({ data, cart }) {
 		const [numPages, setNumPages] = useState(1);
 		const [offset, setOffset] = useState(0);
 		const [perPage, setPerPage] = useState(50);
+		let isLoaded = false;
 		const dataItems = data.items;
 		let itemsWithPictures = [];
+
 		for (let i = 0; i < dataItems.length; i++) {
 			if (dataItems[i].imageId) {
 				itemsWithPictures.push(dataItems[i]);
@@ -22,40 +23,27 @@ export default function ShopCategories({ data, cart }) {
 		}
 
 		const handlePageChange = (e) => {
-			let newOffSet;
-			console.log('Selected Page: ', e.selected);
-			if (e.selected === 0) {
-				newOffSet = 0;
-				setOffset(newOffSet);
-				console.log('Offset: ', offset);
-			} else {
-				newOffSet = e.selected * perPage;
-				console.log('MATH: ', e.selected * perPage);
-				setOffset(newOffSet);
-				console.log('Offset: ', offset);
-			}
-			const currentItems = items.slice(offset, offset + perPage);
-			setCurrItems(currentItems);
-			console.log('New Curr Items: ', currItems);
+			const selectedPage = e.selected;
+			const newOffset = selectedPage * perPage;
+			setOffset(newOffset);
 		};
 
 		useEffect(() => {
-			console.log('THIS RAN Now');
 			setItems(itemsWithPictures);
 			console.log('Items set: ', items);
 			setCurrItems(items.slice(offset, offset + perPage));
 			console.log('Curr Items Set: ', currItems);
 			setNumPages(itemsWithPictures.length / 50);
-		}, [data]);
+		}, []);
 
 		useEffect(() => {
 			setCurrItems(items.slice(offset, offset + perPage));
+			console.log(currItems);
 		}, [offset]);
 		return (
 			<Layout cart={cart} title={`Shop || We Made It`}>
 				<Headers title='Shop' />
 				{/* <CategorySelect /> */}
-				{/* <Pagination currentCursor={currentCursor} name={name} cat={cat} /> */}
 				<div className='w-full flex justify-center'>
 					<ReactPaginate
 						pageCount={numPages}
@@ -69,24 +57,28 @@ export default function ShopCategories({ data, cart }) {
 				</div>
 				<div className='container m-1 lg:m-5 flex flex-row flex-wrap justify-center w-full font-body'>
 					{currItems.map((list, i) => {
-						let price = (
-							items[i].itemData.variations[0].itemVariationData.priceMoney
-								.amount / 100
-						).toFixed(2);
+						let price;
+						if (currItems[i].itemData.variations) {
+							price = (
+								currItems[i].itemData.variations[0].itemVariationData.priceMoney
+									.amount / 100
+							).toFixed(2);
+						} else {
+							price = '$??.??';
+						}
 
 						return (
 							<ProductCards
-								item={items[i]}
-								title={items[i].itemData.name}
-								itemID={items[i].id}
-								key={items[i].id}
+								item={currItems[i]}
+								title={currItems[i].itemData.name}
+								itemID={currItems[i].id}
+								key={currItems[i].id}
 								price={price}
 								defaultImage='/pictureComingSoon.png'
 							/>
 						);
 					})}
 				</div>
-				{/* <Pagination currentCursor={currentCursor} name={name} cat={cat} /> */}
 			</Layout>
 		);
 	} else {
