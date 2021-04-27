8b72f90d5e2310fc707e17672ace89ae6f175c2e
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-04-27 15:59:28 -0400

    Adding Square

--- pages/Shop/shop.js
+++ pages/Shop/shop.js
@@ -4,6 +4,8 @@ import Layout from '../../components/Layout/Layout';
 import Pagination from '../../components/Layout/Pagination';
 import ProductCards from '../../components/Product/ProductCards';
 import { vendors } from '../../VendorList/VendorList';
+import JSONBig from 'json-bigint';
+import { Client, Environment } from 'square';
 
 export default function ShopCategories({
 	itemsWithPictures,
@@ -338,14 +340,68 @@ export default function ShopCategories({
 }
 
 export async function getStaticProps() {
-	const res = await fetch('https://we-made-it-v2.herokuapp.com/newcatalog', {
-		method: 'post',
-		headers: { 'Content-Type': 'application/json' },
+	const client = new Client({
+		environment: Environment.Production,
+		accessToken: process.env.SQUARE_ACCESS_TOKEN, //process.env.SQUARE_ACCESS_TOKEN
 	});
-	const data = await res.json();
-	const itemsWithPictures = data.items;
-	return {
-		props: { itemsWithPictures },
-		revalidate: 3600,
+
+	const recursiveCatalog = async (cursor = '', initialRequest = true) => {
+		let opts = 'ITEM';
+		const catalog = client.catalogApi;
+
+		const response = await catalog.listCatalog(cursor, opts);
+		const data = JSONBig.parse(JSONBig.stringify(response.result.objects));
+		const newCursor = response.result.cursor;
+
+		if (initialRequest && cursor === '') {
+			return data.concat(await recursiveCatalog(newCursor, false));
+		} else if (!initialRequest && !cursor) {
+			return data;
+		} else {
+			return data.concat(await recursiveCatalog(newCursor, false));
+		}
 	};
+
+	const newImageRequest = async (items) => {
+		const catalog = client.catalogApi;
+
+		let newItemsWithPictures = [];
+
+		for (let i = 0; i < items.length; i++) {
+			const response = await catalog.retrieveCatalogObject(items[i].imageId);
+			items[i].imageLink = response.result.object.imageData.url;
+			newItemsWithPictures.push(items[i]);
+		}
+		return newItemsWithPictures;
+	};
+
+	let items = [];
+	let filteredItems = [];
+
+	//This grabs the entire catalog at once through recursion
+	items = await recursiveCatalog();
+	//Then the items are filtered so that only ones that have photo's are returned
+	if (items) {
+		const dataItems = items;
+		for (let i = 0; i < dataItems.length; i++) {
+			if (dataItems[i].imageId) {
+				filteredItems.push(dataItems[i]);
+			}
+		}
+
+		//Finally, before returning the list of Items it grabs the URL for the photo's for each item -- This takes a while!
+		const itemsWithPictures = await newImageRequest(filteredItems);
+
+		return {
+			props: { itemsWithPictures },
+			revalidate: 3600,
+		};
+	}
+
+	// const res = await fetch('https://we-made-it-v2.herokuapp.com/newcatalog', {
+	// 	method: 'post',
+	// 	headers: { 'Content-Type': 'application/json' },
+	// });
+	// const data = await res.json();
+	// const itemsWithPictures = data.items;
 }
