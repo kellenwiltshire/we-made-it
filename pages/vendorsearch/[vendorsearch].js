c9d9959b37014afa5263dd81587b9027bf5c14fd
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-05-15 10:17:15 -0400

    Revalidate on vendor search

---
+++ pages/vendorsearch/[vendorsearch].js
@@ -0,0 +1,187 @@
+import React, { useState, useEffect } from 'react';
+import Headers from '../../components/Layout/Headers';
+import Layout from '../../components/Layout/Layout';
+import ProductCards from '../../components/Product/ProductCards';
+import CategorySelect from '../../components/Categories/CategorySelect';
+import { vendors } from '../../VendorList/VendorList';
+const JSONBig = require('json-bigint');
+const { Client, Environment } = require('square');
+
+export default function SearchItems({ cart, searchresults, vendorSales }) {
+	if (searchresults) {
+		if (searchresults.length) {
+			let results = searchresults;
+
+			const checkCartDiscounts = () => {
+				results.filter((item) => {
+					if (item.itemData.description) {
+						for (let i = 0; i < vendorSales.length; i++) {
+							const lowerCaseVendor = vendorSales[i].vendor.toLowerCase();
+							const lowerCaseItem = item.itemData.description.toLowerCase();
+							if (lowerCaseItem.includes(lowerCaseVendor)) {
+								item.sale = vendorSales[i].sale;
+							}
+						}
+					}
+				});
+			};
+			checkCartDiscounts();
+			return (
+				<Layout cart={cart} title='We Made It'>
+					<div className='flex flex-row flex-wrap justify-center h-full'>
+						<Headers title='Search Results' />
+						<CategorySelect />
+
+						<div className='container m-1 sm:m-5 flex flex-row flex-wrap justify-center w-full'>
+							{results.map((result, i) => {
+								let price;
+								if (result.itemData.variations) {
+									if (
+										result.itemData.variations[0].itemVariationData
+											.pricingType === 'VARIABLE_PRICING'
+									) {
+										price = 'Variable Pricing - Contact Store for Details';
+										return (
+											<ProductCards
+												item={result}
+												title={result.itemData.name}
+												itemID={result.id}
+												price={price}
+												defaultImage='/sparklelogoblack.png'
+												key={Math.random()}
+											/>
+										);
+									} else if (result.sale) {
+										let currPrice =
+											result.itemData.variations[0].itemVariationData.priceMoney
+												.amount / 100;
+										price = currPrice - currPrice * (result.sale / 100);
+										price = price.toFixed(2);
+										return (
+											<ProductCards
+												item={result}
+												title={result.itemData.name}
+												itemID={result.id}
+												salePrice={price}
+												image={result.imageLink}
+												key={Math.random()}
+											/>
+										);
+									} else {
+										price = (
+											result.itemData.variations[0].itemVariationData.priceMoney
+												.amount / 100
+										).toFixed(2);
+										return (
+											<ProductCards
+												item={result}
+												title={result.itemData.name}
+												itemID={result.id}
+												price={price}
+												image={result.imageLink}
+												key={Math.random()}
+											/>
+										);
+									}
+								} else {
+									return;
+								}
+							})}
+						</div>
+					</div>
+				</Layout>
+			);
+		} else {
+			return (
+				<Layout cart={cart} title={`We Made It`}>
+					<Headers title='OOPS! Nothing Was Found!' />
+					<div className='flex flex-col text-center font-body'>
+						<p>
+							Unfortunately nothing was found with that search. Please try
+							searching for something else!
+						</p>
+					</div>
+				</Layout>
+			);
+		}
+	} else {
+		return (
+			<Layout cart={cart} title={`We Made It`}>
+				<Headers title='OOPS! Nothing Was Found!' />
+				<div className='flex flex-col text-center font-body'>
+					<p>
+						Unfortunately nothing was found with that search. Please try
+						searching for something else!
+					</p>
+				</div>
+			</Layout>
+		);
+	}
+}
+
+export async function getStaticPaths() {
+	const vendorList = vendors;
+	const fixedVendors = vendorList.map((ven) => {
+		const fixedVen = ven.vendor.replace(/\s+/g, '%20');
+		return fixedVen;
+	});
+
+	// Get the paths we want to pre-render based on posts
+	const paths = fixedVendors.map((ven) => ({
+		params: { vendorsearch: ven },
+	}));
+
+	// We'll pre-render only these paths at build time.
+	// { fallback: false } means other routes should 404.
+	return { paths, fallback: false };
+}
+
+export async function getStaticProps({ params }) {
+	console.log(params);
+	const client = new Client({
+		environment: Environment.Production,
+		accessToken: process.env.SQUARE_ACCESS_TOKEN,
+	});
+	const search = params.vendorsearch.replace(/%20/g, ' ');
+	console.log('Vendor: ', search);
+
+	let filteredItems = [];
+
+	const newImageRequest = async (items) => {
+		const catalog = client.catalogApi;
+
+		let newItemsWithPictures = [];
+
+		for (let i = 0; i < items.length; i++) {
+			const response = await catalog.retrieveCatalogObject(items[i].imageId);
+			items[i].imageLink = response.result.object.imageData.url;
+			newItemsWithPictures.push(items[i]);
+		}
+		return newItemsWithPictures;
+	};
+	try {
+		const response = await client.catalogApi.searchCatalogItems({
+			textFilter: search,
+		});
+
+		const data = JSONBig.parse(JSONBig.stringify(response.result.items));
+
+		for (let i = 0; i < data.length; i++) {
+			if (data[i].imageId) {
+				filteredItems.push(data[i]);
+			}
+		}
+	} catch (error) {
+		console.log('Search Error: ', error);
+		return {
+			props: {},
+		};
+	}
+
+	const searchresults = await newImageRequest(filteredItems);
+
+	return {
+		props: { searchresults },
+		revalidate: 60,
+	};
+}
