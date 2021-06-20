4c503c59d3b68d5b3f8c71a116b75bcd5bff92cc
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-06-20 16:30:44 -0700

    Adding FB Chat

---
+++ components/Layout/Messenger.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Messenger() {
+	return (
+		<div
+			className='fb-customerchat'
+			attribution='setup_tool'
+			page_id='109237417715566'
+		></div>
+	);
+}
+
+export default Messenger;
