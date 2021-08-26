5166ef7c7bd07df3cf75c77cc328ceb464e79a80
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-08-26 15:53:27 -0400

    Adding Category Id's

--- utils/options.js
+++ utils/options.js
@@ -13,4 +13,20 @@ export const categories = [
 	'Drinkware',
 ];
 
+export const bathAndBodyId = 'ZS6AXRGUSYFSA7UTFBBX2FI6';
+export const babyAndKidsId = '';
+export const accessoriesAndApparelId = '';
+export const foodAndDrinkId = '';
+export const holidaySpecialOccasionId = '';
+export const ecoFriendlyId = '';
+export const cardsAndStationaryId = '';
+export const jewelryId = 'FLJOD6DX4DLOSVAGCQKBFNHA';
+export const homeDecorId = '3WLMCOD626OP6VFKPGIHGX7W';
+export const plantsId = '';
+export const petsId = '';
+export const drinkwareId = '';
+
 export const locations = ['Newcastle', 'Cobourg'];
+
+export const newcastleLocationId = 'L0SCPZY3N0MGA';
+export const cobourgLocationId = 'LQQF7JXRMNY9M';
