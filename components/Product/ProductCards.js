92484f348b7a107a27d1c6c9038db4f143d8054d
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-09-03 15:09:52 -0400

    Reverting product card look

--- components/Product/ProductCards.js
+++ components/Product/ProductCards.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
+import Price from '../Checkout/Price';
 
-function ProductCards({ itemID, title, price, image, salePrice, location }) {
-	const newcastleStore = 'L0SCPZY3N0MGA';
-	const cobourgStore = 'LQQF7JXRMNY9M';
+function ProductCards({ itemID, title, item, price, image, salePrice }) {
 	if (salePrice) {
 		return (
 			<Link
@@ -12,21 +11,19 @@ function ProductCards({ itemID, title, price, image, salePrice, location }) {
 					query: { product: itemID },
 				}}
 			>
-				<a className='flex justify-center flex-wrap bg-white w-36 sm:w-40 md:w-72 md:m-5 m-1 border border-purple-400 rounded'>
-					<div className='border-b-2 border-purple-200 relative'>
-						<img src={image} alt={title} layout='fill' />
+				<a className='flex justify-center flex-wrap bg-white w-36 sm:w-40 md:w-80 cursor-pointer rounded transform hover:shadow-2xl duration-300 ease-in-out md:m-5 border border-purple-400 m-1'>
+					<div className='flex flex-wrap bg-white w-36 sm:w-40 md:w-80 shadow cursor-pointer rounded m-5'>
+						<div>
+							<img src={image} alt='' width='600px' height='auto' />
+						</div>
 					</div>
-					<div className='h-48 relative'>
-						<div className='font-title text-2xl pt-4 px-4 font-semibold'>
+					<div className='flex flex-wrap bg-white w-28 sm:w-40 md:w-80'>
+						<h2 className='text-xs sm:text-base md:text-xl uppercase text-center w-full font-title'>
 							{title}
-						</div>
-
-						<div
-							className='font-body font-medium text-base absolute bottom-0 right-0 mb-4 pl-8 pr-4 pb-1 pt-2 bg-red-200
-            rounded-tl-sm triangle'
-						>
-							<div className=''>Sale: ${salePrice}</div>
-						</div>
+						</h2>
+						<p className='md:ml-5 text-center md:text-left w-full text-xs md:text-base font-body'>
+							Sale Price: ${salePrice}
+						</p>
 					</div>
 				</a>
 			</Link>
@@ -39,21 +36,19 @@ function ProductCards({ itemID, title, price, image, salePrice, location }) {
 					query: { product: itemID },
 				}}
 			>
-				<a className='flex justify-center flex-wrap bg-white w-36 sm:w-40 md:w-72 md:m-5 m-1 border border-purple-400 rounded'>
-					<div className='border-b-2 border-purple-200 relative'>
-						<img src={image} alt={title} width='600px' height='auto' />
+				<a className='flex justify-center flex-wrap bg-white w-36 sm:w-40 md:w-80 cursor-pointer rounded transform hover:shadow-2xl duration-300 ease-in-out md:m-5 border border-purple-400 m-1'>
+					<div className='flex flex-wrap bg-white w-36 sm:w-40 md:w-80 shadow cursor-pointer rounded m-5'>
+						<div>
+							<img src={image} alt='' width='600px' height='auto' />
+						</div>
 					</div>
-					<div className='h-48 relative'>
-						<div className='font-title text-2xl pt-4 px-4 font-semibold'>
+					<div className='flex flex-wrap bg-white w-28 sm:w-40 md:w-80'>
+						<h2 className='text-xs sm:text-base md:text-xl uppercase text-center w-full font-title'>
 							{title}
-						</div>
-
-						<div
-							className='font-body font-medium text-base absolute bottom-0 right-0 mb-4 pl-8 pr-4 pb-1 pt-2 bg-purple-200
-            rounded-tl-sm triangle'
-						>
-							<div className=''>${price}</div>
-						</div>
+						</h2>
+						<p className='md:ml-5 text-center md:text-left w-full text-xs md:text-base font-body'>
+							${price}
+						</p>
 					</div>
 				</a>
 			</Link>
