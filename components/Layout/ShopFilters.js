d7baa17d70ee335069d6599f20b2bd2de57a287b
Kellen Wiltshire <kellenwiltshire@outlook.com>
2021-09-09 16:42:45 -0400

    Categories Fixed

--- components/Layout/ShopFilters.js
+++ components/Layout/ShopFilters.js
@@ -90,7 +90,10 @@ export default function ShopFilters({
 						id='category'
 						className='text-sm md:text-base w-auto mt-2 py-3 px-3 rounded-lg bg-white dark:bg-gray-800 border border-gray-400  text-gray-800 font-semibold focus:border-dark-purple focus:outline-none m-2 font-body'
 						onChange={(e) =>
-							updatePage(categoryChange(e.target.value, items), e.target.value)
+							updatePage(
+								categoryChange(e.target.value, initialItems),
+								e.target.value,
+							)
 						}
 						defaultValue='Category'
 					>
